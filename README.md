abffdeb2c4e78ef2248aae24aef6a625ac7b3fe3
Pedro Ferreira <pedro-ferreira92@hotmail.com>
2021-09-20 17:16:38 +0100

    EZ ALTES BESTA

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # nvidea_cogs
 
+Nvidea, not to be confused with Nvidia, is a group of basement dwellers that aim to create a mediocre version of a voice recognition discord bot.
+
+## Red Bot Cogs Documentation
 https://docs.discord.red/en/stable/guide_cog_creation.html
+
+## Discord.py Documentation
+https://discordpy.readthedocs.io/en/stable/
--- voicerecognition/voicerecognition.py
+++ voicerecognition/voicerecognition.py
@@ -3,17 +3,31 @@ from redbot.core import commands
 class VoiceRecognition(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.active = False
 
-    @commands.command()
     # [p] means command prefix
-    # when user uses [p]join
-    async def join(self, ctx):
-        await ctx.send("I should join user's voice channel here")
-
     # when user uses [p]toggle
+    @commands.command()
     async def toggle(self, ctx):
-        await ctx.send("Voice recognition functionality should start/stop working here")
+        if len(self.bot.voice_clients) == 0:
+            self.active = True
+            author = ctx.message.author
+            channel = author.voice.channel
+            await channel.connect()
+            await ctx.send("Voice recognition enabled.")
+        elif ctx.author.voice.channel == ctx.voice_client.channel:
+            self.active = not self.active
+            await ctx.send("Voice recognition enabled." if self.active else "Voice recognition disabled.")
+        else:
+            await ctx.send("You are not in the same voice channel as me.")
 
-    # when user uses [p]leave
-    async def leave(self, ctx):
-        await ctx.send("I should leave voice channel here")
+    # when user uses [p]exit
+    @commands.command()
+    async def exit(self, ctx):
+        if len(self.bot.voice_clients) == 0:
+            await ctx.send("I am not in a voice channel.")
+        elif ctx.author.voice.channel == ctx.voice_client.channel:
+            await ctx.voice_client.disconnect()
+        else:
+            await ctx.send("You are not in the same voice channel as me.")
+        
