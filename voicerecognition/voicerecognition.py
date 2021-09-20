773ec78e08e2a64dbc41b32966bf66767d8b9ec4
Pedro Ferreira <pedro-ferreira92@hotmail.com>
2021-09-20 17:34:14 +0100

    cleaner code

--- voicerecognition/voicerecognition.py
+++ voicerecognition/voicerecognition.py
@@ -5,29 +5,37 @@ class VoiceRecognition(commands.Cog):
         self.bot = bot
         self.active = False
 
+    async def voiceMiddleware(self, ctx, error, success):
+        if not hasattr(ctx.author.voice, 'channel'):
+            await ctx.send("You are not in a voice channel.")
+        elif len(self.bot.voice_clients) == 0:
+            await error()
+        elif ctx.author.voice.channel == ctx.voice_client.channel:
+            await success()
+        else:
+            await ctx.send("We are not in the same voice channel.")
+
     # [p] means command prefix
     # when user uses [p]toggle
     @commands.command()
     async def toggle(self, ctx):
-        if len(self.bot.voice_clients) == 0:
+        async def error():
             self.active = True
             author = ctx.message.author
             channel = author.voice.channel
             await channel.connect()
             await ctx.send("Voice recognition enabled.")
-        elif ctx.author.voice.channel == ctx.voice_client.channel:
+        async def success():
             self.active = not self.active
             await ctx.send("Voice recognition enabled." if self.active else "Voice recognition disabled.")
-        else:
-            await ctx.send("You are not in the same voice channel as me.")
+        await self.voiceMiddleware(ctx, error, success)
 
     # when user uses [p]exit
     @commands.command()
     async def exit(self, ctx):
-        if len(self.bot.voice_clients) == 0:
+        async def error():
             await ctx.send("I am not in a voice channel.")
-        elif ctx.author.voice.channel == ctx.voice_client.channel:
+        async def success():
             await ctx.voice_client.disconnect()
-        else:
-            await ctx.send("You are not in the same voice channel as me.")
+        await self.voiceMiddleware(ctx, error, success)
         
